ca721a1961f9271c2d50f54361700eef7013c3d1
Peter Mangialardi <petermang122@gmail.com>
2025-03-29 18:51:39 -0400

    fix buffer to pass ci

--- src/core/buffer.zig
+++ src/core/buffer.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const sysinput = @import("root").sysinput;
+const sysinput = @import("../sysinput.zig");
 
 const debug = sysinput.core.debug;
 const insertion = sysinput.text.insertion;
