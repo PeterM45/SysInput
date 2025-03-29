0504edf7b909b8f3b5fa441509da1704cdb39b77
Peter Mangialardi <petermang122@gmail.com>
2025-03-29 17:58:31 -0400

    Update buffer.zig

--- src/core/buffer.zig
+++ src/core/buffer.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const sysinput = @import("../sysinput.zig");
+const sysinput = @import("root").sysinput;
 
 const debug = sysinput.core.debug;
 const insertion = sysinput.text.insertion;
